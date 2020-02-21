106238c8475c9b4a4c4c4233a616bd67164e1165
emailseghen@gmail.com <Kahid3lu!>
2020-02-20 23:25:57 -0500

    new

--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
 console.log("Coding can be difficult")
+<<<<<<< HEAD
 console.log("but git makes it easier!")
+=======
+console.log("but git makes it easier")
+>>>>>>> 8ea2e4fa3df37bc4cd57ad78a4bd80bac0a953a2
